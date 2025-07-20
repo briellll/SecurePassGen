12426363c32d16a1f0ee66fd28626ded4ab09d60
Gabriel <carloscgmorais@gmail.com>
2025-07-20 19:49:46 -0300

    fix: Correct password_created key typo in test and clean up code

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,21 +1,23 @@
-from http import HTTPStatus
-
 from fastapi.testclient import TestClient
-
 from securepassgen.main import app
+from http import HTTPStatus
 
 client = TestClient(app)
 
+def test_endpoint_create_password_ok():
+    payload = {
+        'length': 15,
+        'add_number': True,
+        'add_symbol': True
+    }
 
-def test_endpoint_create_passwords_ok():
-    payload = {'length': 14, 'add_number': True, 'add_symbol': True}
-
-    response = client.post('/gerar-senha', json=payload)
+    response = client.post('/gerar-senha',json=payload)
 
     assert response.status_code == HTTPStatus.OK
 
     data = response.json()
     assert 'password_created' in data
-    assert isinstance(data['password_created'], str)
+    assert isinstance(data['password_crated'],str)
     assert len(data['password_created']) == payload['length']
     assert data['parameter'] == payload
+    
